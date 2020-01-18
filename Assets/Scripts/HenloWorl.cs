5139f6246d05041974dcd6cda583c305f9e4648d
Temulgeh <56922769+Temulgeh@users.noreply.github.com>
2020-01-18 11:46:15 +0100

    Added a Henlo Worl script
    Just verifying commiting works.

---
+++ Assets/Scripts/HenloWorl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenloWorl : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Henlo Worl!");
+    }
+}
