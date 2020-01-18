c00324777374fd3c62730a90bd6788981388c725
Temulgeh <56922769+Temulgeh@users.noreply.github.com>
2020-01-19 00:17:57 +0100

    Made a moving box test
    Or why Unity's 2D physics aren't what we need:
    - The box sometimes gets stuck when moving along a wall (can probably be fixed by changing the collider type)
    - The player sometimes bounces off the wall (integration error? Bounciness parameter that I couldn't find?)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float acceleration = 1f;
+    [SerializeField]
+    private float friction = .9f;
+    private Rigidbody2D rb;
+    private Vector2 tempVel;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (!rb)
+        {
+            Debug.Log("Couldn't find a Rigidbody2D in " + gameObject.name);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        tempVel = rb.velocity; // In case something else changes the velocity
+        if (Input.GetKey(KeyCode.UpArrow))
+            tempVel.y += acceleration;
+        if (Input.GetKey(KeyCode.DownArrow))
+            tempVel.y -= acceleration;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            tempVel.x -= acceleration;
+        if (Input.GetKey(KeyCode.RightArrow))
+            tempVel.x += acceleration;
+        tempVel *= friction;
+        rb.velocity = tempVel;
+    }
+}
