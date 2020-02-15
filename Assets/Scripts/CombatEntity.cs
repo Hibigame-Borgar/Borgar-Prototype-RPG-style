d58f2954d659849a6402465e3487aeeb26f5ccc5
Temulgeh <56922769+Temulgeh@users.noreply.github.com>
2020-02-15 19:27:06 +0100

    Started to implement the combat system
    But I'm not sure how exactly I want to implement it, so I'll wait for you to give your opinion

---
+++ Assets/Scripts/CombatEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatEntity : MonoBehaviour
+{
+    // Made everything public because why not
+    public int startingHealth;
+    public int basicAttack;
+    public int basicDefense;
+
+    public int health;
+    public int attack;  // To take in account buffs and debuffs,
+    public int defense; // will implement later
+
+    public CombatManager manager;
+
+    public void Start()
+    {
+        attack = basicAttack;
+        defense = basicDefense;
+        health = startingHealth;
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage - defense;
+        if (health <= 0)
+        {
+            manager.kill(this);
+        }
+    }
+}
